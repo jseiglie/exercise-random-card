654afd287f9987f15d4c6528d4ad8415d55ebea3
jseiglie <javierseiglie@gmail.com>
2022-01-20 15:13:10 +0000

    ramdom card deployed

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const heart = document.querySelector('.heart');
 const cub = document.querySelector('.cub');
 const spade = document.querySelector('.spade');
 const diamond = document.querySelector('.diamond');
+const cardUp = document.querySelector('.cardUp');
+const cardDown = document.querySelector('.cardDown')
+const card = document.querySelector('.card')
 //dataset
 const cardType = ['♦', '♥', '♠', '♣'];
 const cardNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
@@ -25,6 +28,15 @@ console.log(GetType(RandomIndexCT(cardType), cardType))
 
 
 
+
+
 window.onload = function() {
  cardNum.innerHTML = GetNumber(RandomIndex(cardNumber), cardNumber);
+ cardUp.innerHTML = GetType(RandomIndexCT(cardType), cardType);
+ cardDown.innerHTML = cardUp.innerHTML;
+ if (cardUp.innerHTML== '♥' || cardUp.innerHTML== '♦') {
+    card.classList.add('card-red');
+} else {
+    card.classList.add('card-black');
+}
 }
